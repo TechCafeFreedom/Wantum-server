413c9f45ca6751305c196f2b0465a89e8fbc360b
akubi0w1 <yawn.yawn.yawn.84@gmail.com>
2020-07-30 23:43:38 +0900

    ddlのフォーマット

--- mysql/init.d/ddl.sql
+++ mysql/init.d/ddl.sql
@@ -6,29 +6,29 @@ SET foreign_key_checks=0;
 
 -- define table
 CREATE TABLE IF NOT EXISTS users(
-  id int NOT NULL AUTO_INCREMENT,
-  auth_id varchar(128) NOT NULL UNIQUE,
-  user_name varchar(20) NOT NULL UNIQUE,
-  mail varchar(256) NOT NULL UNIQUE,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  id         int          NOT NULL AUTO_INCREMENT,
+  auth_id    varchar(128) NOT NULL UNIQUE,
+  user_name  varchar(20)  NOT NULL UNIQUE,
+  mail       varchar(256) NOT NULL UNIQUE,
+  created_at datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   deleted_at datetime,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS profiles(
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(50) NOT NULL,
-  thumbnail varchar(2048) NOT NULL,
-  bio varchar(100) NOT NULL COMMENT '自己紹介',
-  gender int NOT NULL COMMENT '1=man, 2=woman, 3=other',
-  phone varchar(15) NOT NULL COMMENT '電話番号',
-  place varchar(30) NOT NULL COMMENT '現在地',
-  birth date DEFAULT NULL,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  id         int           NOT NULL AUTO_INCREMENT,
+  name       varchar(50)   NOT NULL,
+  thumbnail  varchar(2048) NOT NULL,
+  bio        varchar(100)  NOT NULL COMMENT '自己紹介',
+  gender     int           NOT NULL COMMENT '1=man, 2=woman, 3=other',
+  phone      varchar(15)   NOT NULL COMMENT '電話番号',
+  place      varchar(30)   NOT NULL COMMENT '現在地',
+  birth      date          DEFAULT NULL,
+  created_at datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   deleted_at datetime,
-  user_id int NOT NULL,
+  user_id    int           NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT `fk_profile_user`
   	FOREIGN KEY (user_id)
@@ -38,14 +38,14 @@ CREATE TABLE IF NOT EXISTS profiles(
 );
 
 CREATE TABLE IF NOT EXISTS wish_lists(
-  id int NOT NULL AUTO_INCREMENT,
-  title varchar(30) NOT NULL,
+  id                   int           NOT NULL AUTO_INCREMENT,
+  title                varchar(30)   NOT NULL,
   background_image_url varchar(2048) NOT NULL,
-  invite_url varchar(2048) NOT NULL,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  deleted_at datetime,
-  user_id int NOT NULL,
+  invite_url           varchar(2048) NOT NULL,
+  created_at           datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at           datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  deleted_at           datetime,
+  user_id              int           NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT `fk_wish_lists_user`
   	FOREIGN KEY (user_id)
@@ -55,12 +55,12 @@ CREATE TABLE IF NOT EXISTS wish_lists(
 );
 
 CREATE TABLE IF NOT EXISTS categories(
-  id int NOT NULL AUTO_INCREMENT,
-  title varchar(30) NOT NULL,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  deleted_at datetime,
-  wish_list_id int NOT NULL,
+  id           int         NOT NULL AUTO_INCREMENT,
+  title        varchar(30) NOT NULL,
+  created_at   datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at   datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  deleted_at   datetime,
+  wish_list_id int         NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT `fk_categories_wish_list`
     FOREIGN KEY (wish_list_id)
@@ -70,17 +70,17 @@ CREATE TABLE IF NOT EXISTS categories(
 );
 
 CREATE TABLE IF NOT EXISTS wish_cards(
-  id int NOT NULL AUTO_INCREMENT,
-  activity varchar(50) NOT NULL COMMENT 'places.nameでactivityしたい',
+  id          int          NOT NULL AUTO_INCREMENT,
+  activity    varchar(50)  NOT NULL COMMENT 'places.nameでactivityしたい',
   description varchar(500) NOT NULL,
-  date datetime NOT NULL,
-  done_at datetime,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  deleted_at datetime,
-  user_id int NOT NULL,
-  category_id int NOT NULL,
-  place_id int NOT NULL,
+  date        datetime     NOT NULL,
+  done_at     datetime,
+  created_at  datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at  datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  deleted_at  datetime,
+  user_id     int          NOT NULL,
+  category_id int          NOT NULL,
+  place_id    int          NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT `fk_wish_cards_user`
     FOREIGN KEY (user_id)
@@ -100,13 +100,13 @@ CREATE TABLE IF NOT EXISTS wish_cards(
 );
 
 CREATE TABLE IF NOT EXISTS albums(
-  id int NOT NULL AUTO_INCREMENT,
-  title varchar(30) NOT NULL,
+  id         int           NOT NULL AUTO_INCREMENT,
+  title      varchar(30)   NOT NULL,
   invite_url varchar(2048) NOT NULL,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  created_at datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   deleted_at datetime,
-  user_id int NOT NULL,
+  user_id    int           NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT `fk_albums_user`
     FOREIGN KEY (user_id)
@@ -116,16 +116,16 @@ CREATE TABLE IF NOT EXISTS albums(
 );
 
 CREATE TABLE IF NOT EXISTS memories(
-  id int NOT NULL AUTO_INCREMENT,
-  date datetime NOT NULL,
-  activity varchar(50) NOT NULL COMMENT 'places.nameでactivityした',
+  id          int          NOT NULL AUTO_INCREMENT,
+  date        datetime     NOT NULL,
+  activity    varchar(50)  NOT NULL COMMENT 'places.nameでactivityした',
   description varchar(500) NOT NULL,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  deleted_at datetime,
-  user_id int NOT NULL,
-  place_id int NOT NULL,
-  album_id int NOT NULL,
+  created_at  datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at  datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  deleted_at  datetime,
+  user_id     int          NOT NULL,
+  place_id    int          NOT NULL,
+  album_id    int          NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT `fk_memories_user`
     FOREIGN KEY (user_id)
@@ -145,29 +145,29 @@ CREATE TABLE IF NOT EXISTS memories(
 );
 
 CREATE TABLE IF NOT EXISTS places(
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(200) NOT NULL,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  id         int          NOT NULL AUTO_INCREMENT,
+  name       varchar(200) NOT NULL,
+  created_at datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   deleted_at datetime,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS tags(
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(100) NOT NULL UNIQUE,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  id         int          NOT NULL AUTO_INCREMENT,
+  name       varchar(100) NOT NULL UNIQUE,
+  created_at datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   deleted_at datetime,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS photos(
-  id int NOT NULL AUTO_INCREMENT,
-  photo_url varchar(2048) NOT NULL,
-  memory_id int NOT NULL,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  id         int           NOT NULL AUTO_INCREMENT,
+  photo_url  varchar(2048) NOT NULL,
+  memory_id  int           NOT NULL,
+  created_at datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   deleted_at datetime,
   PRIMARY KEY (id),
   CONSTRAINT `fk_photos_memory`
@@ -179,8 +179,8 @@ CREATE TABLE IF NOT EXISTS photos(
 
 -- define relation
 CREATE TABLE IF NOT EXISTS users_wish_lists(
-  id int NOT NULL AUTO_INCREMENT,
-  user_id int NOT NULL,
+  id           int NOT NULL AUTO_INCREMENT,
+  user_id      int NOT NULL,
   wish_list_id int NOT NULL,
   PRIMARY KEY (id),
   UNIQUE (user_id, wish_list_id),
@@ -198,8 +198,8 @@ CREATE TABLE IF NOT EXISTS users_wish_lists(
 COMMENT = 'wish_listsの参加メンバー';
 
 CREATE TABLE IF NOT EXISTS users_albums(
-  id int NOT NULL AUTO_INCREMENT,
-  user_id int NOT NULL,
+  id       int NOT NULL AUTO_INCREMENT,
+  user_id  int NOT NULL,
   album_id int NOT NULL,
   PRIMARY KEY (id),
   UNIQUE  (user_id, album_id),
@@ -217,9 +217,9 @@ CREATE TABLE IF NOT EXISTS users_albums(
 COMMENT = 'albumsの参加メンバー';
 
 CREATE TABLE IF NOT EXISTS wish_cards_tags(
-  id int NOT NULL AUTO_INCREMENT,
+  id           int NOT NULL AUTO_INCREMENT,
   wish_card_id int NOT NULL,
-  tag_id int NOT NULL,
+  tag_id       int NOT NULL,
   PRIMARY KEY (id),
   UNIQUE (wish_card_id, tag_id),
   CONSTRAINT
@@ -235,9 +235,9 @@ CREATE TABLE IF NOT EXISTS wish_cards_tags(
 );
 
 CREATE TABLE IF NOT EXISTS memories_tags(
-  id int NOT NULL AUTO_INCREMENT,
+  id        int NOT NULL AUTO_INCREMENT,
   memory_id int NOT NULL,
-  tag_id int NOT NULL,
+  tag_id    int NOT NULL,
   PRIMARY KEY (id),
   UNIQUE (memory_id, tag_id),
   CONSTRAINT
