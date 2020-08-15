bac6d43fdb883088103346fda6c74605da72f5c8
Takumaron <tennis.takumax@gmail.com>
2020-08-15 12:09:55 +0900

    tlogパッケージのメンバ関数としてログ出力の関数を定義した

--- pkg/tlog/logger.go
+++ pkg/tlog/logger.go
@@ -1,6 +1,13 @@
 package tlog
 
-import "go.uber.org/zap"
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+	"wantum/pkg/constants"
+
+	"go.uber.org/zap"
+)
 
 // アプリケーションログ用のロガー
 var appLogger *zap.Logger
@@ -18,4 +25,18 @@ func init() {
 // getter
 func GetAppLogger() *zap.Logger {
 	return appLogger
+}
+
+func PrintLogWithUID(r *http.Request, err error) {
+	// どこで起きたエラーかを特定するための情報を取得
+	pt, file, line, _ := runtime.Caller(1)
+	funcName := runtime.FuncForPC(pt).Name()
+
+	// エラーログ出力
+	uid, ok := r.Context().Value(constants.AuthCtxKey).(string)
+	if !ok {
+		GetAppLogger().Error(fmt.Sprintf("<[Unknown]Error:%+v, File: %s:%d, Function: %s>", err, file, line, funcName))
+	} else {
+		GetAppLogger().Error(fmt.Sprintf("<[%s]Error:%+v, File: %s:%d, Function: %s>", uid, err, file, line, funcName))
+	}
 }
