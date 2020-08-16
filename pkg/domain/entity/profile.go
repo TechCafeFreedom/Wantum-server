f9748dea9c5ccefafb071de158fc18b08088aa18
Takumaron <tennis.takumax@gmail.com>
2020-08-16 13:42:38 +0900

    Entity定義をUserとProfileでファイルに分けた

---
+++ pkg/domain/entity/profile.go
@@ -0,0 +1,16 @@
+package entity
+
+import "time"
+
+type Profile struct {
+	Name      string
+	Thumbnail string
+	Bio       string
+	Gender    int
+	Phone     string
+	Place     string
+	Birth     string
+	CreatedAt *time.Time
+	UpdatedAt *time.Time
+	DeletedAt *time.Time
+}
--- pkg/domain/entity/user.go
+++ pkg/domain/entity/user.go
@@ -14,16 +14,3 @@ type User struct {
 }
 
 type UserSlice []*User
-
-type Profile struct {
-	Name      string
-	Thumbnail string
-	Bio       string
-	Gender    int
-	Phone     string
-	Place     string
-	Birth     string
-	CreatedAt *time.Time
-	UpdatedAt *time.Time
-	DeletedAt *time.Time
-}
