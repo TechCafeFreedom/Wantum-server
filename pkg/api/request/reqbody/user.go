354a8300f078164dbae8cc77137bd0a97a2c6143
Takumaron <tennis.takumax@gmail.com>
2020-08-17 17:32:14 +0900

    requestBodyの構造体定義にjson修飾子を付与した

--- pkg/api/request/reqbody/user.go
+++ pkg/api/request/reqbody/user.go
@@ -1,14 +1,14 @@
 package reqbody
 
 type UserCreate struct {
-	AuthID    string
-	UserName  string
-	Mail      string
-	Name      string
-	Thumbnail string
-	Bio       string
-	Gender    int
-	Phone     string
-	Place     string
-	Birth     string
+	AuthID    string `json:"auth_id"`
+	UserName  string `json:"user_name"`
+	Mail      string `json:"mail"`
+	Name      string `json:"name"`
+	Thumbnail string `json:"thumbnail"`
+	Bio       string `json:"bio"`
+	Gender    int    `json:"gender"`
+	Phone     string `json:"phone"`
+	Place     string `json:"place"`
+	Birth     string `json:"birth"`
 }
