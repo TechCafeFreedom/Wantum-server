e53ba2d8da644d16b048aeef3952231d7e17c532
Takumaron <tennis.takumax@gmail.com>
2020-08-24 17:40:17 +0900

    userResponseのプロフィール情報はprofileオブジェクトとして表現せずに、プロパティとして定義した

--- pkg/api/response/user.go
+++ pkg/api/response/user.go
@@ -5,11 +5,19 @@ import (
 )
 
 type UserResponse struct {
-	ID       int          `json:"id"`
-	AuthID   string       `json:"auth_id"`
-	UserName string       `json:"user_name"`
-	Mail     string       `json:"mail"`
-	Profile  *UserProfile `json:"profile"`
+	ID       int    `json:"id"`
+	AuthID   string `json:"auth_id"`
+	UserName string `json:"user_name"`
+	Mail     string `json:"mail"`
+
+	// Profileデータ
+	Name      string `json:"name"`
+	Thumbnail string `json:"thumbnail"`
+	Bio       string `json:"bio"`
+	Gender    int    `json:"gender"`
+	Phone     string `json:"phone"`
+	Place     string `json:"place"`
+	Birth     string `json:"birth"`
 }
 
 type UsersResponse []*UserResponse
@@ -36,7 +44,8 @@ func ConvertToUserResponse(userData *entity.User) *UserResponse {
 	// nilチェック
 	if userData == nil {
 		return nil
-	} else if userData.Profile == nil {
+	}
+	if userData.Profile == nil {
 		return &UserResponse{
 			ID:       userData.ID,
 			AuthID:   userData.AuthID,
@@ -46,18 +55,16 @@ func ConvertToUserResponse(userData *entity.User) *UserResponse {
 	}
 
 	return &UserResponse{
-		ID:       userData.ID,
-		AuthID:   userData.AuthID,
-		UserName: userData.UserName,
-		Mail:     userData.Mail,
-		Profile: &UserProfile{
-			Name:      userData.Profile.Name,
-			Thumbnail: userData.Profile.Thumbnail,
-			Bio:       userData.Profile.Bio,
-			Gender:    userData.Profile.Gender,
-			Phone:     userData.Profile.Phone,
-			Place:     userData.Profile.Place,
-			Birth:     userData.Profile.Birth,
-		},
+		ID:        userData.ID,
+		AuthID:    userData.AuthID,
+		UserName:  userData.UserName,
+		Mail:      userData.Mail,
+		Name:      userData.Profile.Name,
+		Thumbnail: userData.Profile.Thumbnail,
+		Bio:       userData.Profile.Bio,
+		Gender:    userData.Profile.Gender,
+		Phone:     userData.Profile.Phone,
+		Place:     userData.Profile.Place,
+		Birth:     userData.Profile.Birth,
 	}
 }
