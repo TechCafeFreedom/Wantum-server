55d4cf14d73ea225710b4e374c00f1d2400e525f
akubi0w1 <yawn.yawn.yawn.84@gmail.com>
2020-08-30 17:50:34 +0900

    place repository: createの戻り値を変更

--- pkg/domain/repository/place/repository.go
+++ pkg/domain/repository/place/repository.go
@@ -6,9 +6,9 @@ import (
 	"wantum/pkg/infrastructure/mysql/model"
 )
 
+// implement: wantum/pkg/infrastructure/mysql/place:placeRepositoryImpletent
 type Repository interface {
-	// TODO: intでよくね？
-	Insert(ctx context.Context, masterTx repository.MasterTx, place *model.PlaceModel) (*model.PlaceModel, error)
+	Insert(ctx context.Context, masterTx repository.MasterTx, place *model.PlaceModel) (int, error)
 
 	Update(ctx context.Context, masterTx repository.MasterTx, place *model.PlaceModel) error
 
