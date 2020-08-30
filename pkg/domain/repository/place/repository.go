49db043552e94f138b255bb53067759732571ccf
akubi0w1 <yawn.yawn.yawn.84@gmail.com>
2020-08-30 14:54:19 +0900

    add place repository

---
+++ pkg/domain/repository/place/repository.go
@@ -0,0 +1,17 @@
+package place
+
+import (
+	"context"
+	"wantum/pkg/domain/repository"
+	"wantum/pkg/infrastructure/mysql/model"
+)
+
+type Repository interface {
+	// TODO: intでよくね？
+	Insert(ctx context.Context, masterTx repository.MasterTx, place *model.PlaceModel) (*model.PlaceModel, error)
+
+	Update(ctx context.Context, masterTx repository.MasterTx, place *model.PlaceModel) error
+	Delete(ctx context.Context, masterTx repository.MasterTx, placeID int) error
+	SelectByID(ctx context.Context, masterTx repository.MasterTx, placeID int) (*model.PlaceModel, error)
+	SelectAll(ctx context.Context, masterTx repository.MasterTx) (model.PlaceModelSlice, error)
+}
