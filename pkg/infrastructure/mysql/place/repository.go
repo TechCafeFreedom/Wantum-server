7f182d07198a2c5a7b0683a81c03f4ab6854eeb6
akubi0w1 <yawn.yawn.yawn.84@gmail.com>
2020-08-30 14:54:19 +0900

    add place repository

---
+++ pkg/infrastructure/mysql/place/repository.go
@@ -0,0 +1,166 @@
+package place
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"wantum/pkg/domain/repository"
+	"wantum/pkg/domain/repository/place"
+	"wantum/pkg/infrastructure/mysql"
+	"wantum/pkg/infrastructure/mysql/model"
+	"wantum/pkg/tlog"
+	"wantum/pkg/werrors"
+)
+
+type placeRepositoryImplement struct {
+	masterTxManager repository.MasterTxManager
+}
+
+func New(txManager repository.MasterTxManager) place.Repository {
+	return &placeRepositoryImplement{
+		masterTxManager: txManager,
+	}
+}
+
+func (repo *placeRepositoryImplement) Insert(ctx context.Context, masterTx repository.MasterTx, place *model.PlaceModel) (*model.PlaceModel, error) {
+	if place == nil {
+		return nil, werrors.Newf(
+			errors.New("required data(place) is nil"),
+			werrors.ServerError.ErrorCode,
+			werrors.ServerError.ErrorMessageJP,
+			werrors.ServerError.ErrorMessageEN,
+		)
+	}
+	tx, err := mysql.ExtractTx(masterTx)
+	if err != nil {
+		tlog.PrintErrorLogWithCtx(ctx, err)
+		return nil, werrors.FromConstant(err, werrors.ServerError)
+	}
+	result, err := tx.Exec(`
+		INSERT INTO places(
+			name, created_at, updated_at
+		) VALUES (?, ?, ?)
+	`, place.Name,
+		place.CreatedAt,
+		place.UpdatedAt,
+	)
+	if err != nil {
+		tlog.PrintErrorLogWithCtx(ctx, err)
+		return nil, werrors.FromConstant(err, werrors.ServerError)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		tlog.PrintErrorLogWithCtx(ctx, err)
+		return nil, werrors.FromConstant(err, werrors.ServerError)
+	}
+	place.ID = int(id)
+	return place, nil
+}
+
+func (repo *placeRepositoryImplement) Update(ctx context.Context, masterTx repository.MasterTx, place *model.PlaceModel) error {
+	if place == nil {
+		return werrors.Newf(
+			errors.New("required data(place) is nil"),
+			werrors.ServerError.ErrorCode,
+			werrors.ServerError.ErrorMessageJP,
+			werrors.ServerError.ErrorMessageEN,
+		)
+	}
+	tx, err := mysql.ExtractTx(masterTx)
+	if err != nil {
+		tlog.PrintErrorLogWithCtx(ctx, err)
+		return werrors.FromConstant(err, werrors.ServerError)
+	}
+	_, err = tx.Exec(`
+		UPDATE places
+		SET name=?, updated_at=?, deleted_at=?
+		WHERE id=?
+	`, place.Name,
+		place.UpdatedAt,
+		place.DeletedAt,
+		place.ID,
+	)
+	if err != nil {
+		tlog.PrintErrorLogWithCtx(ctx, err)
+		return werrors.FromConstant(err, werrors.ServerError)
+	}
+	return nil
+}
+
+func (repo *placeRepositoryImplement) Delete(ctx context.Context, masterTx repository.MasterTx, placeID int) error {
+	tx, err := mysql.ExtractTx(masterTx)
+	if err != nil {
+		tlog.PrintErrorLogWithCtx(ctx, err)
+		return werrors.FromConstant(err, werrors.ServerError)
+	}
+	_, err = tx.Exec(`
+		DELETE FROM places
+		WHERE id=?
+	`, placeID)
+	if err != nil {
+		tlog.PrintErrorLogWithCtx(ctx, err)
+		return werrors.FromConstant(err, werrors.ServerError)
+	}
+	return nil
+}
+
+func (repo *placeRepositoryImplement) SelectByID(ctx context.Context, masterTx repository.MasterTx, placeID int) (*model.PlaceModel, error) {
+	tx, err := mysql.ExtractTx(masterTx)
+	if err != nil {
+		tlog.PrintErrorLogWithCtx(ctx, err)
+		return nil, werrors.FromConstant(err, werrors.ServerError)
+	}
+	row := tx.QueryRow(`
+		SELECT id, name, created_at, updated_at, deleted_at
+		FROM places
+		WHERE id=?
+	`, placeID)
+	var result model.PlaceModel
+	err = row.Scan(
+		&result.ID,
+		&result.Name,
+		&result.CreatedAt,
+		&result.UpdatedAt,
+		&result.DeletedAt)
+	if err != nil {
+		tlog.PrintErrorLogWithCtx(ctx, err)
+		return nil, werrors.FromConstant(err, werrors.ServerError)
+	}
+	return &result, nil
+}
+
+func (repo *placeRepositoryImplement) SelectAll(ctx context.Context, masterTx repository.MasterTx) (model.PlaceModelSlice, error) {
+	tx, err := mysql.ExtractTx(masterTx)
+	if err != nil {
+		tlog.PrintErrorLogWithCtx(ctx, err)
+		return nil, werrors.FromConstant(err, werrors.ServerError)
+	}
+	rows, err := tx.Query(`
+		SELECT id, name, created_at, updated_at, deleted_at
+		FROM places
+	`)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		tlog.PrintErrorLogWithCtx(ctx, err)
+		return nil, werrors.FromConstant(err, werrors.ServerError)
+	}
+	var result model.PlaceModelSlice
+	for rows.Next() {
+		var place model.PlaceModel
+		err = rows.Scan(
+			&place.ID,
+			&place.Name,
+			&place.CreatedAt,
+			&place.UpdatedAt,
+			&place.DeletedAt)
+		if err != nil {
+			tlog.PrintErrorLogWithCtx(ctx, err)
+			return nil, werrors.FromConstant(err, werrors.ServerError)
+		}
+		result = append(result, &place)
+	}
+	return result, nil
+}
---
+++ pkg/infrastructure/mysql/place/repository_test.go
@@ -0,0 +1,226 @@
+package place
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"log"
+	"os"
+	"testing"
+	"time"
+	"wantum/pkg/domain/repository"
+	"wantum/pkg/domain/repository/place"
+	tx "wantum/pkg/infrastructure/mysql"
+	"wantum/pkg/infrastructure/mysql/model"
+
+	"github.com/stretchr/testify/assert"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+var txManager repository.MasterTxManager
+var repo place.Repository
+
+func TestMain(m *testing.M) {
+	before()
+	code := m.Run()
+	after()
+	os.Exit(code)
+}
+
+// repositoryを作ってもらう
+func before() {
+	var err error
+	db, err = sql.Open("mysql", "root:root@tcp(localhost:3306)/wantum?parseTime=true")
+	if err != nil {
+		log.Fatal("faild to connect db: ", err)
+	}
+	txManager = tx.NewDBMasterTxManager(db)
+	repo = New(txManager)
+}
+
+// dbのコネクションを閉じる
+func after() {
+	db.Close()
+}
+
+func TestInsert(t *testing.T) {
+	t.Run("success to insert data", func(t *testing.T) {
+		var err error
+		ctx := context.Background()
+		date := time.Date(2020, 9, 1, 12, 0, 0, 0, time.Local)
+		place := &model.PlaceModel{
+			Name:      "sample place",
+			CreatedAt: &date,
+			UpdatedAt: &date,
+		}
+
+		var result *model.PlaceModel
+		err = txManager.Transaction(ctx, func(ctx context.Context, masterTx repository.MasterTx) error {
+			result, err = repo.Insert(ctx, masterTx, place)
+			return err
+		})
+
+		assert.NoError(t, err)
+		assert.NotNil(t, result)
+		assert.NotNil(t, result.ID)
+	})
+
+	t.Run("failed to insert data. data is nil", func(t *testing.T) {
+		var err error
+		ctx := context.Background()
+
+		var result *model.PlaceModel
+		err = txManager.Transaction(ctx, func(ctx context.Context, masterTx repository.MasterTx) error {
+			result, err = repo.Insert(ctx, masterTx, nil)
+			return err
+		})
+
+		assert.Error(t, err)
+		assert.Nil(t, result)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("success to update data", func(t *testing.T) {
+		var err error
+		ctx := context.Background()
+		date := time.Date(2020, 9, 1, 12, 0, 0, 0, time.Local)
+		place := &model.PlaceModel{
+			ID:        1,
+			Name:      "sample place",
+			CreatedAt: &date,
+			UpdatedAt: &date,
+		}
+
+		var result *model.PlaceModel
+		err = txManager.Transaction(ctx, func(ctx context.Context, masterTx repository.MasterTx) error {
+			err = repo.Update(ctx, masterTx, place)
+			if err != nil {
+				return err
+			}
+
+			result, err = repo.SelectByID(ctx, masterTx, 1)
+			return err
+		})
+
+		assert.NoError(t, err)
+		assert.Equal(t, "sample place", result.Name)
+	})
+
+	t.Run("failure to update data. data is nil", func(t *testing.T) {
+		var err error
+		ctx := context.Background()
+
+		var result *model.PlaceModel
+		err = txManager.Transaction(ctx, func(ctx context.Context, masterTx repository.MasterTx) error {
+			err = repo.Update(ctx, masterTx, nil)
+			if err != nil {
+				return err
+			}
+
+			result, err = repo.SelectByID(ctx, masterTx, 1)
+			return err
+		})
+
+		assert.Error(t, err)
+		assert.Nil(t, result)
+
+	})
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("success to delete", func(t *testing.T) {
+		var err error
+		ctx := context.Background()
+		date := time.Date(2020, 9, 1, 12, 0, 0, 0, time.Local)
+		place := &model.PlaceModel{
+			Name:      "sample place",
+			CreatedAt: &date,
+			UpdatedAt: &date,
+		}
+
+		var result *model.PlaceModel
+		err = txManager.Transaction(ctx, func(ctx context.Context, masterTx repository.MasterTx) error {
+			result, _ = repo.Insert(ctx, masterTx, place)
+
+			err = repo.Delete(ctx, masterTx, result.ID)
+			if err != nil {
+				return err
+			}
+
+			result, err = repo.SelectByID(ctx, masterTx, result.ID)
+			if err == nil {
+				return errors.New("削除されたデータが引っかかった")
+			}
+			return nil
+		})
+
+		assert.NoError(t, err)
+		assert.Nil(t, result)
+	})
+}
+
+func TestSelectByID(t *testing.T) {
+	t.Run("success to select by id", func(t *testing.T) {
+		var err error
+		ctx := context.Background()
+		date := time.Date(2020, 9, 1, 12, 0, 0, 0, time.Local)
+		place := &model.PlaceModel{
+			Name:      "sample place",
+			CreatedAt: &date,
+			UpdatedAt: &date,
+		}
+
+		var result *model.PlaceModel
+		err = txManager.Transaction(ctx, func(ctx context.Context, masterTx repository.MasterTx) error {
+			newData, _ := repo.Insert(ctx, masterTx, place)
+
+			result, err = repo.SelectByID(ctx, masterTx, newData.ID)
+			return err
+		})
+
+		assert.NoError(t, err)
+		assert.NotNil(t, result)
+	})
+
+	t.Run("failure to select by id. id is not exist", func(t *testing.T) {
+		var err error
+		ctx := context.Background()
+		date := time.Date(2020, 9, 1, 12, 0, 0, 0, time.Local)
+		place := &model.PlaceModel{
+			Name:      "sample place",
+			CreatedAt: &date,
+			UpdatedAt: &date,
+		}
+
+		var result *model.PlaceModel
+		err = txManager.Transaction(ctx, func(ctx context.Context, masterTx repository.MasterTx) error {
+			newData, _ := repo.Insert(ctx, masterTx, place)
+			repo.Delete(ctx, masterTx, newData.ID)
+
+			result, err = repo.SelectByID(ctx, masterTx, newData.ID)
+			return err
+		})
+
+		assert.Error(t, err)
+		assert.Nil(t, result)
+	})
+}
+
+func TestSelectAll(t *testing.T) {
+	t.Run("success to select all", func(t *testing.T) {
+		var err error
+		ctx := context.Background()
+
+		var result model.PlaceModelSlice
+		err = txManager.Transaction(ctx, func(ctx context.Context, masterTx repository.MasterTx) error {
+			result, err = repo.SelectAll(ctx, masterTx)
+			return err
+		})
+
+		assert.NoError(t, err)
+		assert.NotNil(t, result)
+	})
+}
