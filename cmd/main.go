5ebcdedc78c568764ea6669e60366c3361e94bf3
Takumaron <tennis.takumax@gmail.com>
2020-09-07 13:43:45 +0900

    RESTサーバーとgRPCどちらも同時に起動する様にmain関数を修正した

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"wantum/db/mysql"
-	"wantum/pkg/api/middleware"
+	grpcMiddleware "wantum/pkg/api/middleware/grpc"
+	restMiddleware "wantum/pkg/api/middleware/rest"
 	tx "wantum/pkg/infrastructure/mysql"
 	"wantum/pkg/tlog"
 
-	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
-	"github.com/rs/cors"
 )
 
 func main() {
@@ -26,45 +24,15 @@ func main() {
 	// トランザクションマネージャーの作成
 	masterTxManager := tx.NewDBMasterTxManager(dbInstance)
 
-	// APIインスタンスの作成
-	userAPI := InitUserAPI(masterTxManager)
-
 	// firebase middlewareの作成
-	firebaseClient := middleware.CreateFirebaseInstance()
+	restFirebaseClient := restMiddleware.CreateFirebaseInstance()
+	grpcFirebaseClient := grpcMiddleware.CreateFirebaseInstance()
 
-	// CORS対応
-	c := cors.New(
-		cors.Options{
-			AllowedHeaders:   []string{"*"},
-			AllowedOrigins:   []string{"*"},
-			AllowedMethods:   []string{"*"},
-			AllowCredentials: true,
-		},
-	)
-	r := mux.NewRouter()
+	// RESTサーバの立ち上げ
+	go func() {
+		StartRestAPIServer(restFirebaseClient, masterTxManager)
+	}()
 
-	// connection testAPI
-	r.HandleFunc("/ping/{hoge}/{fuga}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		fmt.Fprintf(w, "Hello %v, world! %v", vars["hoge"], vars["fuga"])
-	})
-
-	// userAPI
-	firebaseAuth := r.PathPrefix("").Subrouter()
-	r.HandleFunc("/users", userAPI.GetAllUsers).Methods("GET")
-
-	firebaseAuth.Use(firebaseClient.MiddlewareFunc)
-	{
-		firebaseAuth.HandleFunc("/users", userAPI.CreateNewUser).Methods("POST")
-		firebaseAuth.HandleFunc("/users/self", userAPI.GetAuthorizedUser).Methods("GET")
-	}
-
-	// port: 8080
-	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: c.Handler(r),
-	}
-	if err := srv.ListenAndServe(); err != nil {
-		tlog.GetAppLogger().Fatal(err.Error())
-	}
+	// gRPCサーバーの立ち上げ
+	StartGrpcServer(grpcFirebaseClient, masterTxManager)
 }
